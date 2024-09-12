4cd9234df006e0a584ba2e344e52e1dc1cdbd589
sohee-local <sohui0718@naver.com>
2024-09-12 12:56:37 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 (function () {
- const template = document.createElement('template')
- template.innerHTML = `
- <style>
- </style>
- <div id="root" style="width: 100%; height: 100%;">
- Hello WebComponent
- </div>
- `
- class Main extends HTMLElement {
- constructor () {
- super()
- this._shadowRoot = this.attachShadow({ mode: 'open' })
- this._shadowRoot.appendChild(template.content.cloneNode(true))
- this._root = this._shadowRoot.getElementById('root')
- }
- }
- customElements.define('sohee-exercise-001-main', Main)
- })()
+   const template = document.createElement('template')
+   template.innerHTML = `
+   <style>
+   </style>
+   <div id="root" style="width: 100%; height: 100%;">
+   Hello WebComponent
+   </div>
+   `
+   class Main extends HTMLElement {
+   constructor () {
+   super()
+   this._shadowRoot = this.attachShadow({ mode: 'open' })
+   this._shadowRoot.appendChild(template.content.cloneNode(true))
+   this._root = this._shadowRoot.getElementById('root')
+   }
+   onCustomWidgetResize (width, height) {
+      this.render()
+   }
+  
+   onCustomWidgetAfterUpdate (changedProps) {
+   }
+  
+   onCustomWidgetDestroy () {
+   }
+  
+   render () {
+      this._root.textContent = `Hello Custom Widget clientWidth: ${this.clientWidth}, clientHeight: ${this.clientHeight} `
+   }
+   }
+   customElements.define('sohee-exercise-001-main', Main)
+   })()
